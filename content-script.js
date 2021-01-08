7d49c6d4abccc2b5289b0064171cb8b36ba377d0
Daniil7davidov <daniil7davidov@gmail.com>
2021-01-08 16:18:21 +0500

    добавили сообщение в расширение

--- content-script.js
+++ content-script.js
@@ -7,8 +7,23 @@ function myMain(evt) {
     if (document.querySelector("#side header")) {
       clearInterval(jsInitChecktimer);
       // DO YOUR STUFF HERE.
-      let help = document.createElement("div");
-      help.append("help");
+      // let inp = document.createElement("input")
+      // inp.style.display = "none";
+      // document.appendChild(inp)
+
+      let messages = ["привет", 'отвечу чуть позже', 'наберу через 15 мин)']
+
+      let help = document.createElement("ul");
+      for (let i = 0; i < messages.length; i++){
+        console.log(messages[i])
+        let message = document.createElement("li")
+        message.append(messages[i])
+        message.style.margin = "15px";
+        message.onclick = function () {
+          console.log(123);
+        };
+        help.appendChild(message)
+    }
       help.style.cursor = "pointer";
       help.onclick = function () {
         console.log(123);
